f0b2b3fb45a30e1427f6d4340776415265413672
Brandt Wright <wright.brandt@gmail.com>
2024-08-14 13:00:39 -0400

    fix(nvim): disable Treesitter for Haskell files
    This (temporary) fix disables Treesitter for Haskell files. This is
    necessitated by the fact that TS causes nvim to crash when opening my
    XMonad configuration file. This allows for the editing (and cleanup) of
    XMonad configuration within nvim but should be removed after XMonad
    configuration has been cleaned up and so that TS can be made available
    when editing Haskell files.

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -8,6 +8,11 @@ return {
             vim.notify("Treesitter disabled due to file size")
             return true
           end
+          -- TODO: clean up xmonad config and reenable TS for Haskell files
+          if vim.bo.filetype == "haskell" then
+            vim.notify("Treesitter disabled due to file type")
+            return true
+          end
         end,
       },
     },
