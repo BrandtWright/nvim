a8124bd389fd62f07c1b308fe2fffb7c64633f47
Brandt Wright <wright.brandt@gmail.com>
2024-08-17 13:20:45 -0400

    remove treesitter clamp for Haskell files

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -8,11 +8,6 @@ return {
             vim.notify("Treesitter disabled due to file size")
             return true
           end
-          -- TODO: clean up xmonad config and reenable TS for Haskell files
-          if vim.bo.filetype == "haskell" then
-            vim.notify("Treesitter disabled due to file type")
-            return true
-          end
         end,
       },
     },
