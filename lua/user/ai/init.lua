3514226fda2e1380ec5cc66fc8222c2afde94dd8
Brandt Wright <wright.brandt@gmail.com>
2024-08-21 14:12:12 -0400

    feat(chatgpt): add keybinds for `:ChatGPTRun`

--- lua/user/ai/init.lua
+++ lua/user/ai/init.lua
@@ -28,6 +28,86 @@ return {
         end,
         desc = "Open",
       },
+      {
+        "<leader>ae",
+        function()
+          vim.cmd("ChatGPTRun explain_code")
+        end,
+        desc = "Explain Code",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>ag",
+        function()
+          vim.cmd("ChatGPTRun grammar_correction")
+        end,
+        desc = "Grammar Check",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>ak",
+        function()
+          vim.cmd("ChatGPTRun keywords")
+        end,
+        desc = "Keywords",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>ad",
+        function()
+          vim.cmd("ChatGPTRun docstring")
+        end,
+        desc = "Doc String",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>at",
+        function()
+          vim.cmd("ChatGPTRun add_tests")
+        end,
+        desc = "Add Tests",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>ao",
+        function()
+          vim.cmd("ChatGPTRun optimize_code")
+        end,
+        desc = "Add Tests",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>as",
+        function()
+          vim.cmd("ChatGPTRun summarize")
+        end,
+        desc = "Summarize",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>af",
+        function()
+          vim.cmd("ChatGPTRun fix_bugs")
+        end,
+        desc = "Fix Bugs",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>aR",
+        function()
+          vim.cmd("ChatGPTRun roxygen_edit")
+        end,
+        desc = "Fix Bugs",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>ar",
+        function()
+          vim.cmd("ChatGPTRun code_readability_analysis")
+        end,
+        desc = "Code Readability Analysis",
+        mode = { "n", "x" },
+      },
     },
     cmd = {
       "ChatGPT",
