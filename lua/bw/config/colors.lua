9853b8d929e08702b51a1c0b2f328d14c290c816
Brandt Wright <wright.brandt@gmail.com>
2024-08-24 02:17:03 -0400

    pretty(color scheme): update colors

--- lua/bw/config/colors.lua
+++ lua/bw/config/colors.lua
@@ -1,43 +1,58 @@
 local colors = {
 
-  white = "#ebdbb2",
-  black = "#181815",
-  red = "#bd7671",
-  green = "#769482",
-  yellow = "#c2c27c",
-  blue = "#5692db",
-  magenta = "#8f7099",
-  cyan = "#7b919e",
-
   gray = "#99968e",
 
+  white = "#CCBEA3",
+  black = "#12110E",
+  red = "#8F5454",
+  orange = "#8F7054",
+  yellow = "#8F8F5B",
+  green = "#728F7D",
+  cyan = "#54898F",
+  blue = "#395A8F",
+  purple = "#54548F",
+  magenta = "#7B548F",
+  pink = "#8F547B",
+
   -- Bright Colors
-  bright_white = "#fff2d1",
-  bright_black = "#575757",
-  bright_red = "#ffa099",
-  bright_green = "#b9d9b8",
-  bright_yellow = "#fcfca4",
-  bright_blue = "#77b3fc",
-  bright_magenta = "#c99fd6",
-  bright_cyan = "#a7cacc",
+  birght_white = "#FFEECC",
+  birght_black = "#454037",
+  birght_red = "#DB8181",
+  birght_orange = "#DBAB81",
+  birght_yellow = "#DBDB8C",
+  birght_green = "#AFDBC1",
+  birght_cyan = "#81D2DB",
+  birght_blue = "#629AF5",
+  birght_purple = "#9090F5",
+  birght_magenta = "#D390F5",
+  birght_pink = "#F590D3",
+
+  -- Dark Colors
+  dark_white = "#B3A78F",
+  dark_black = "#0A0908",
+  dark_red = "#422727",
+  dark_orange = "#423427",
+  dark_yellow = "#42422A",
+  dark_green = "#35423A",
+  dark_cyan = "#274042",
+  dark_blue = "#1B2A42",
+  dark_purple = "#272742",
+  dark_magenta = "#392742",
+  dark_pink = "#422739",
 
   -- Other colors
 
   teal = "#468187",
 
   light_orange = "#c9925a",
-  orange = "#ce9178",
-  dark_orange = "#735244",
 
   pale_green = "#57804d",
   other_green = "#9ac95b",
 
   light_gold = "#C5B358",
 
-  dark_red = "#663f3f",
   really_dark_red = "#331717",
 
-  dark_blue = "#2e4d66",
   really_dark_blue = "#2e4d66",
   terminal_blue = "#637ca6",
 
@@ -48,14 +63,13 @@ local colors = {
   gold = "#a38e5d",
 
   -- UI Stuff
-  comment = "#575757",
-  cursor_line = "#1a1a1a",
+  comment = "#333029",
+  cursor_line = "#141310",
   visual = "#2e2e2c",
-  terminal = "#171717",
+  terminal = "#12110E",
   statusline = "#3c3836",
 
   -- Duplicates
-  purple = "#8a7b9e",
   light_purple = "#eebaff",
 }
 
