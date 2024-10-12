8bd91a0e3edae49e4d64261bad3f459ceba71b93
Brandt Wright <wright.brandt@gmail.com>
2024-10-12 11:52:37 -0400

    feat(completion): fix up completion a bit.

--- lua/user/completion/init.lua
+++ lua/user/completion/init.lua
@@ -10,6 +10,9 @@ end
 return {
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      "f3fora/cmp-spell",
+    },
     opts = function(_, opts)
       opts = opts or {}
       opts.preselect = cmp.PreselectMode.None
@@ -48,9 +51,77 @@ return {
         end,
       })
       opts.experimental = {}
+
+      -- opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "spell", option = { keep_all_entries = true }, keyword_length = 1 })
+
+      opts.sources = cmp.config.sources({
+        -- { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lsp" },
+        { name = "path" },
+        {
+          name = "buffer",
+          option = {
+            get_bufnrs = function()
+              return vim.api.nvim_list_bufs()
+            end,
+          },
+          keyword_length = 5,
+        },
+        {
+          name = "spell",
+          keyword_length = 5,
+          option = {
+            keep_all_entries = true,
+          },
+        },
+      })
+
+      opts.formatting = {
+        format = function(entry, item)
+          if icons[item.kind] then
+            item.kind = " " .. icons[item.kind]
+          end
+          item.menu = ({
+            nvim_lsp = "[LSP]",
+            path = "[PTH]",
+            buffer = "[BUF]",
+            spell = "[SPL]",
+          })[entry.source.name]
+          return item
+        end,
+      }
+
+      opts.sorting = {
+        comparators = {
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+
+          -- copied from cmp-under, but probably don't need the whole plugin.
+          -- May add more custom comparators.
+          function(entry1, entry2)
+            local _, entry1_under = entry1.completion_item.label:find("^_+")
+            local _, entry2_under = entry2.completion_item.label:find("^_+")
+            entry1_under = entry1_under or 0
+            entry2_under = entry2_under or 0
+            if entry1_under > entry2_under then
+              return false
+            elseif entry1_under < entry2_under then
+              return true
+            end
+          end,
+
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      }
     end,
   },
 }
+
 -- return {
 --   {
 --     "hrsh7th/nvim-cmp",
