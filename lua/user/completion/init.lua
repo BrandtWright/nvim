dd8cd5c668be57cdc1dc5759eb631ef85c159fa5
Brandt Wright <wright.brandt@gmail.com>
2024-10-12 17:06:30 -0400

    refac(completion): clean up nvim-cmp a bit

--- lua/user/completion/init.lua
+++ lua/user/completion/init.lua
@@ -1,19 +1,30 @@
-local cmp = require("cmp")
-local icons = require("lazyvim.config").icons.kinds
-local colors = require("bw.config.colors")
-vim.cmd("hi CmpItemAbbrMatch guifg=" .. colors.red)
-for k, _ in pairs(icons) do
-  local cmd = string.format("hi CmpItemKind%s guifg=%s", k, colors.gold)
-  vim.cmd(cmd)
-end
-
 return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      icons = {
+        kinds = {
+          Snippet = "Տ ",
+        },
+      },
+    },
+  },
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "f3fora/cmp-spell",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
     },
     opts = function(_, opts)
+      local cmp = require("cmp")
+      local colors = require("bw.config.colors")
+
+      -- Configure Colors
+      vim.cmd("hi CmpItemAbbrMatch guifg=" .. colors.bright_green)
+      vim.cmd("hi CmpItemAbbrMatchFuzzy guifg=" .. colors.white)
+      vim.cmd("hi CmpItemKindFunction guifg=" .. colors.blue)
+      vim.cmd("hi CmpItemKindSnippet guifg=" .. colors.green)
+
       opts = opts or {}
       opts.preselect = cmp.PreselectMode.None
 
@@ -52,72 +63,17 @@ return {
       })
       opts.experimental = {}
 
-      -- opts.sources = opts.sources or {}
-      table.insert(opts.sources, { name = "spell", option = { keep_all_entries = true }, keyword_length = 1 })
-
-      opts.sources = cmp.config.sources({
-        -- { name = "nvim_lsp_signature_help" },
-        { name = "nvim_lsp" },
-        { name = "path" },
-        {
-          name = "buffer",
-          option = {
-            get_bufnrs = function()
-              return vim.api.nvim_list_bufs()
-            end,
-          },
-          keyword_length = 5,
-        },
-        {
-          name = "spell",
-          keyword_length = 5,
-          option = {
-            keep_all_entries = true,
-          },
-        },
+      -- Configure additional sources
+      table.insert(opts.sources, {
+        name = "spell",
+        option = { keep_all_entries = true },
+        keyword_length = 5,
       })
-
-      opts.formatting = {
-        format = function(entry, item)
-          if icons[item.kind] then
-            item.kind = " " .. icons[item.kind]
-          end
-          item.menu = ({
-            nvim_lsp = "[LSP]",
-            path = "[PTH]",
-            buffer = "[BUF]",
-            spell = "[SPL]",
-          })[entry.source.name]
-          return item
-        end,
-      }
-
-      opts.sorting = {
-        comparators = {
-          cmp.config.compare.offset,
-          cmp.config.compare.exact,
-          cmp.config.compare.score,
-
-          -- copied from cmp-under, but probably don't need the whole plugin.
-          -- May add more custom comparators.
-          function(entry1, entry2)
-            local _, entry1_under = entry1.completion_item.label:find("^_+")
-            local _, entry2_under = entry2.completion_item.label:find("^_+")
-            entry1_under = entry1_under or 0
-            entry2_under = entry2_under or 0
-            if entry1_under > entry2_under then
-              return false
-            elseif entry1_under < entry2_under then
-              return true
-            end
-          end,
-
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-        },
-      }
+      table.insert(opts.sources, {
+        name = "nvim_lsp_signature_help",
+        option = { keep_all_entries = true },
+        keyword_length = 5,
+      })
     end,
   },
 }
