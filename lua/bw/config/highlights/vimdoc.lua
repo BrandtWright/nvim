cad53af625130adfe6e968feff0b5ffd44369b00
Brandt Wright <wright.brandt@gmail.com>
2024-10-14 15:46:13 -0400

    pretty(vimdocs): better colors

--- lua/bw/config/highlights/vimdoc.lua
+++ lua/bw/config/highlights/vimdoc.lua
@@ -1,10 +1,11 @@
 local c = require("bw.config.colors")
 
 return {
-  { name = "@label.vimdoc", fg = c.gold },
-  { name = "@markup.heading.1.vimdoc", fg = c.violet, bg = c.terminal, styles = {} },
+  { name = "@label.vimdoc", fg = c.yellow },
+  { name = "@markup.heading.1.vimdoc", fg = c.gold, bg = c.terminal, styles = {} },
   { name = "@markup.link.vimdoc", fg = c.green, bg = c.terminal, styles = {} },
   { name = "@variable.parameter.vimdoc", fg = c.green, bg = c.terminal, styles = {} },
   { name = "@keyword.exception.vim", fg = c.red, bg = c.terminal, styles = {} },
-  { name = "@string.special.url.vimdoc", fg = c.blue, bg = c.terminal, styles = {} },
+  { name = "@string.special.url.vimdoc", fg = c.bright_magenta, bg = c.terminal, styles = {} },
+  { name = "@markup.link.vimdoc", fg = c.bright_magenta, bg = c.terminal, styles = {} },
 }
