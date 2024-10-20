ca4fc0aed2f1c69e9810fdda31ea0012343b9b60
Brandt Wright <wright.brandt@gmail.com>
2024-10-20 14:34:37 -0400

    pretty(markdown): change heading highlights

--- lua/user/filetypes/markdown.lua
+++ lua/user/filetypes/markdown.lua
@@ -1,12 +1,12 @@
 local c = require("bw.config.colors")
 local highlights = require("bw.config.highlights")
 highlights.register({
-  { name = "RenderMarkdownH1Bg", fg = c.terminal, bg = c.dark_red, styles = { "b" } },
-  { name = "RenderMarkdownH2Bg", fg = c.terminal, bg = c.dark_orange, styles = { "b" } },
-  { name = "RenderMarkdownH3Bg", fg = c.terminal, bg = c.gold, styles = { "b" } },
-  { name = "RenderMarkdownH4Bg", fg = c.terminal, bg = c.violet, styles = { "b" } },
-  { name = "RenderMarkdownH5Bg", fg = c.terminal, bg = c.blue, styles = { "b" } },
-  { name = "RenderMarkdownH6Bg", fg = c.terminal, bg = c.cyan, styles = { "b" } },
+  { name = "RenderMarkdownH1Bg", fg = c.terminal, bg = c.yellow, styles = { "b" } },
+  { name = "RenderMarkdownH2Bg", fg = c.terminal, bg = c.gold, styles = { "b" } },
+  { name = "RenderMarkdownH3Bg", fg = c.terminal, bg = c.orange, styles = { "b" } },
+  { name = "RenderMarkdownH4Bg", fg = c.terminal, bg = c.red, styles = { "b" } },
+  { name = "RenderMarkdownH5Bg", fg = c.terminal, bg = c.dark_red, styles = { "b" } },
+  { name = "RenderMarkdownH6Bg", fg = c.terminal, bg = c.brown, styles = { "b" } },
   { name = "RenderMarkdownBullet", fg = c.gold, bg = c.terminal },
   { name = "RenderMarkdownChecked", fg = c.green, styles = {} },
   { name = "RenderMarkdownUnchecked", fg = c.red, styles = {} },
