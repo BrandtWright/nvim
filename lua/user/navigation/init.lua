d4e3030a28f57427543c8ee957758c6bb1ffd6b8
Brandt Wright <wright.brandt@gmail.com>
2024-11-23 23:46:36 -0500

    feat(neotree): leave netrw alone

--- lua/user/navigation/init.lua
+++ lua/user/navigation/init.lua
@@ -31,6 +31,7 @@ return {
         bind_to_cwd = true,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        hijack_netrw_behavior = "disabled",
       },
     },
   },
