5423ab1b8d448c75037a4b5f68276a02bab6523e
Brandt Wright <wright.brandt@gmail.com>
2025-03-02 16:22:53 -0500

    docs(language-services/treesitter): add todo for large files

--- lua/user/language-services/init.lua
+++ lua/user/language-services/init.lua
@@ -15,6 +15,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       highlight = {
+        -- TODO: Compare with current lazyvim implementation for large files
         disable = function(_, buf)
           if require("bw.util.fs").buff_exceeds_size(buf, 1024 * 1024 * 5) then
             vim.notify("Treesitter disabled due to file size")
