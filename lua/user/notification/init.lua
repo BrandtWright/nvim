ed259a47bed38c06fe1aa51a2d2d7f9c1d597499
Brandt Wright <wright.brandt@gmail.com>
2025-03-02 16:45:35 -0500

    fix(noice): fix command line pop-up highlights

--- lua/user/notification/init.lua
+++ lua/user/notification/init.lua
@@ -1,13 +1,14 @@
-local c = require("bw.config.colors")
-local highlights = require("bw.config.highlights")
-highlights.register({
-  { name = "NoiceCmdlinePopupBorder", fg = c.gold, bg = c.terminal, styles = {} },
-  { name = "NoiceCmdlineIcon", fg = c.gold, bg = c.terminal, styles = {} },
-  { name = "NoiceCmdlinePopupTitle", fg = c.gold, bg = c.terminal, styles = {} },
-})
-
 return {
   {
     "folke/noice.nvim",
+    opts = function()
+      local c = require("bw.config.colors")
+      local highlights = require("bw.config.highlights")
+      highlights.register({
+        { name = "NoiceCmdlinePopupBorder", fg = c.gold, bg = c.terminal, styles = {} },
+        { name = "NoiceCmdlineIcon", fg = c.gold, bg = c.terminal, styles = {} },
+        { name = "NoiceCmdlinePopupTitle", fg = c.gold, bg = c.terminal, styles = {} },
+      })
+    end,
   },
 }
