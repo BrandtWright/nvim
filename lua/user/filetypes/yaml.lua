d4f31b727c4942f7210d0313f8fdea6a53ea380d
Brandt Wright <wright.brandt@gmail.com>
2025-03-03 21:29:19 -0500

    docs(ft/yaml): update docs for customTags

--- lua/user/filetypes/yaml.lua
+++ lua/user/filetypes/yaml.lua
@@ -10,11 +10,11 @@ return {
                 -- Ensure that Ansible vault tags don't cause linting errors in
                 -- yaml files. This is a bit annoying in Ansible variable
                 -- definition files that do not contain any valid Ansible syntax.
-                -- In this case, ft=yaml and ft!=yaml.ansible so, will not cause
-                -- ansiblels to attatch to the yaml buffer. Adding the !vault
-                -- scalar to customTags prevents yamlls from complaining about
-                -- variable definitions that use ansible-vault to store
-                -- encrypted strings in Ansible projects.
+                -- In this case, ft=yaml and ft!=yaml.ansible (set by nvim-ansible)
+                -- so, will not cause ansiblels to attatch to the yaml buffer.
+                -- Adding the !vault scalar to customTags prevents yamlls from
+                -- complaining about variable definitions that use ansible-vault
+                -- to store encrypted strings in Ansible projects.
                 "!vault scalar",
               },
             },
