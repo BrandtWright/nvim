b49e1524d1106bd909dadc7f1146091f634addbb
Brandt Wright <wright.brandt@gmail.com>
2025-03-05 11:51:34 -0500

    refac(ft/ansible): clean up ansible config

--- lua/user/filetypes/ansible.lua
+++ lua/user/filetypes/ansible.lua
@@ -1,3 +1,13 @@
+local instrument_ansible_buffer = function()
+  -- Set up buffer-local key map for `ansible-lint`
+  vim.keymap.set("n", "<localleader>a", function()
+    Snacks.terminal.open("ansible-lint " .. vim.fn.expand("%") .. " && anykey", {
+      win = { border = "single", style = "terminal", relative = "editor", width = 120, height = 25 },
+      interactive = true,
+    })
+  end, { buffer = true, noremap = true, silent = true, desc = "Ansible Lint Buffer" })
+end
+
 return {
   {
     "williamboman/mason.nvim",
@@ -6,28 +16,10 @@ return {
       opts = opts or {}
       opts.ensure_installed = opts.ensure_installed or {}
       table.insert(opts.ensure_installed, "ansible-lint")
-
-      -- Set up a buf-local keymap for ansible-list
       vim.api.nvim_create_autocmd("FileType", {
-        group = vim.api.nvim_create_augroup("AnsibleKeyMaps", { clear = true }),
+        group = vim.api.nvim_create_augroup("AnsibleLintKeyMapAutoGroup", { clear = true }),
         pattern = "yaml.ansible",
-        callback = function()
-          local keymap_opts = { buffer = true, noremap = true, silent = true, desc = "Ansible Lint Buffer" }
-          vim.keymap.set("n", "<localleader>a", function()
-            local filepath = vim.fn.expand("%")
-            local cmd = "ansible-lint " .. filepath .. " && anykey"
-            Snacks.terminal.open(cmd, {
-              win = {
-                border = "single",
-                style = "terminal",
-                relative = "editor",
-                width = 120,
-                height = 25,
-              },
-              interactive = true,
-            })
-          end, keymap_opts)
-        end,
+        callback = instrument_ansible_buffer,
       })
     end,
   },
