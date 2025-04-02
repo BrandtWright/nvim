0a982d78da052920bf3f3543fec6454be96f810d
Brandt Wright <wright.brandt@gmail.com>
2025-04-02 00:44:59 -0400

    pretty(snacks.picker): improved highlights

--- lua/dist/snacks.lua
+++ lua/dist/snacks.lua
@@ -1,3 +1,10 @@
+local c = require("bw.config.colors")
+local highlights = require("bw.config.highlights")
+highlights.register({
+  { name = "SnacksPickerMatch", fg = c.green },
+  { name = "SnacksPickerCursorLine", bg = c.cursorline },
+})
+
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("DisableSnacksIndentGuides", { clear = true }),
   pattern = "markdown",
