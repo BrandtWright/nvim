f14b8e9a81f2f696bd2cced1a1c9eb34a8668dd5
Brandt Wright <wright.brandt@gmail.com>
2025-04-03 19:14:14 -0400

    refac(snacks): move snacks picker config to snacks module

--- lua/dist/lazy/init.lua
+++ lua/dist/lazy/init.lua
@@ -25,41 +25,6 @@ return {
         end,
         desc = "Lazy Config",
       },
-      {
-        "<leader>lpa",
-        function()
-          require("snacks").picker.picker_actions()
-        end,
-        desc = "Actions",
-      },
-      {
-        "<leader>lpf",
-        function()
-          require("snacks").picker.picker_format()
-        end,
-        desc = "Format",
-      },
-      {
-        "<leader>lpl",
-        function()
-          require("snacks").picker.picker_layouts()
-        end,
-        desc = "Layouts",
-      },
-      {
-        "<leader>lpp",
-        function()
-          require("snacks").picker.picker_preview()
-        end,
-        desc = "Preview",
-      },
-      {
-        "<leader>sd",
-        function()
-          require("snacks").picker.diagnostics()
-        end,
-        desc = "Diagnostics",
-      },
     },
   },
 }
--- lua/dist/snacks/picker.lua
+++ lua/dist/snacks/picker.lua
@@ -34,11 +34,46 @@ return {
         desc = "Highlights",
       },
       {
+        "<leader>sd",
+        function()
+          require("snacks").picker.diagnostics()
+        end,
+        desc = "Diagnostics",
+      },
+      {
         "<leader>fS",
         function()
           require("snacks").picker.lsp_config()
         end,
         desc = "LSP Config",
+      },
+      {
+        "<leader>lpa",
+        function()
+          require("snacks").picker.picker_actions()
+        end,
+        desc = "Actions",
+      },
+      {
+        "<leader>lpf",
+        function()
+          require("snacks").picker.picker_format()
+        end,
+        desc = "Format",
+      },
+      {
+        "<leader>lpl",
+        function()
+          require("snacks").picker.picker_layouts()
+        end,
+        desc = "Layouts",
+      },
+      {
+        "<leader>lpp",
+        function()
+          require("snacks").picker.picker_preview()
+        end,
+        desc = "Preview",
       },
     },
   },
