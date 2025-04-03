76785448dc679bc572da732234869eaad35ee65f
Brandt Wright <wright.brandt@gmail.com>
2025-04-02 23:17:41 -0400

    feat(snacks): add snacks.picker key binds

--- lua/dist/snacks/indent.lua
+++ lua/dist/snacks/indent.lua
@@ -1,10 +1,6 @@
-local c = require("bw.config.colors")
-local highlights = require("bw.config.highlights")
-highlights.register({
-  { name = "SnacksPickerMatch", fg = c.green },
-  { name = "SnacksPickerCursorLine", bg = c.cursorline },
-})
-
+--------------------------------------------------------------------------------
+-- Indent
+--------------------------------------------------------------------------------
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("DisableSnacksIndentGuides", { clear = true }),
   pattern = "markdown",
---
+++ lua/dist/snacks/picker.lua
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+-- Picker
+--------------------------------------------------------------------------------
+local c = require("bw.config.colors")
+local highlights = require("bw.config.highlights")
+highlights.register({
+  { name = "SnacksPickerMatch", fg = c.green },
+  { name = "SnacksPickerListCursorline", bg = c.cursorline },
+})
+
+return {
+  {
+    "folke/snacks.nvim",
+    keys = {
+      {
+        "<leader>si",
+        function()
+          require("snacks").picker.icons({ layout = { preview = false, preset = "dropdown" } })
+        end,
+        desc = "Icons",
+      },
+      {
+        "<leader>cs",
+        function()
+          require("snacks").picker.lsp_symbols()
+        end,
+        desc = "Symbols (Document)",
+      },
+    },
+  },
+}
