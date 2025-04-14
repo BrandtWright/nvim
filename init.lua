cd3dfeaa68aaa4b973a3674309ada3d8a7fdf7c8
Brandt Wright <wright.brandt@gmail.com>
2025-04-14 14:42:05 -0400

    refac(terminal colors): remove unnecessary comments

--- init.lua
+++ init.lua
@@ -64,20 +64,20 @@ require("lazy").setup({
   },
 })
 local c = require("bw.config.colors")
-vim.g.terminal_color_0 = c.black -- black
-vim.g.terminal_color_1 = c.red -- red
-vim.g.terminal_color_2 = c.green -- green
-vim.g.terminal_color_3 = c.yellow -- yellow
-vim.g.terminal_color_4 = c.blue -- blue
-vim.g.terminal_color_5 = c.magenta -- magenta
-vim.g.terminal_color_6 = c.cyan -- cyan
-vim.g.terminal_color_7 = c.white -- white
+vim.g.terminal_color_0 = c.black
+vim.g.terminal_color_1 = c.red
+vim.g.terminal_color_2 = c.green
+vim.g.terminal_color_3 = c.yellow
+vim.g.terminal_color_4 = c.blue
+vim.g.terminal_color_5 = c.magenta
+vim.g.terminal_color_6 = c.cyan
+vim.g.terminal_color_7 = c.white
 
-vim.g.terminal_color_8 = c.bright_black -- black
-vim.g.terminal_color_9 = c.bright_red -- red
-vim.g.terminal_color_10 = c.bright_green -- green
-vim.g.terminal_color_11 = c.bright_yellow -- yellow
-vim.g.terminal_color_12 = c.bright_blue -- blue
-vim.g.terminal_color_13 = c.bright_magenta -- magenta
-vim.g.terminal_color_14 = c.bright_cyan -- cyan
-vim.g.terminal_color_15 = c.bright_white -- white
+vim.g.terminal_color_8 = c.bright_black
+vim.g.terminal_color_9 = c.bright_red
+vim.g.terminal_color_10 = c.bright_green
+vim.g.terminal_color_11 = c.bright_yellow
+vim.g.terminal_color_12 = c.bright_blue
+vim.g.terminal_color_13 = c.bright_magenta
+vim.g.terminal_color_14 = c.bright_cyan
+vim.g.terminal_color_15 = c.bright_white
