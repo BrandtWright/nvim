65f5cd0f7199e9d64b7466bc0a8251b277b17ac7
Brandt Wright <wright.brandt@gmail.com>
2025-04-13 21:26:45 -0400

    refac(ansible module): better encapsulation

--- lua/user/filetypes/ansible.lua
+++ lua/user/filetypes/ansible.lua
@@ -1,26 +1,29 @@
-local instrument_ansible_buffer = function()
-  -- Set up buffer-local key map for `ansible-lint`
-  vim.keymap.set("n", "<localleader>a", function()
-    Snacks.terminal.open("ansible-lint " .. vim.fn.expand("%") .. " && anykey", {
-      win = { border = "single", style = "terminal", relative = "editor", width = 120, height = 25 },
-      interactive = true,
-    })
-  end, { buffer = true, noremap = true, silent = true, desc = "Ansible Lint Buffer" })
-end
-
 return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      -- Ensure ansible is installed
-      opts = opts or {}
-      opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, "ansible-lint")
+      local instrument_ansible_buffer = function()
+        -- Set up buffer-local key map for `ansible-lint`
+        vim.keymap.set("n", "<localleader>a", function()
+          Snacks.terminal.open("ansible-lint " .. vim.fn.expand("%") .. " && anykey", {
+            win = { border = "single", style = "terminal", relative = "editor", width = 120, height = 25 },
+            interactive = true,
+          })
+        end, { buffer = true, noremap = true, silent = true, desc = "Ansible Lint Buffer" })
+      end
+
+      -- Set up buffer local keymaps for ansible files
       vim.api.nvim_create_autocmd("FileType", {
         group = vim.api.nvim_create_augroup("AnsibleLintKeyMapAutoGroup", { clear = true }),
         pattern = "yaml.ansible",
         callback = instrument_ansible_buffer,
       })
+
+      local my_opts = {
+        ensure_installed = { "ansible-lint" },
+      }
+
+      return vim.tbl_deep_extend("force", opts or {}, my_opts)
     end,
   },
 }
