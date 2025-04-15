fff346711cda46928f331ba0eb0a4059c04e6d1e
Brandt Wright <wright.brandt@gmail.com>
2025-04-15 19:38:16 -0400

    refac(dist): highlight groups -> plugin spec

--- lua/bw/config/highlights/lazy.lua
+++
@@ -1,6 +0,0 @@
-local c = require("bw.config.colors")
-return {
-  { name = "LazyNormal", fg = nil, bg = c.terminal, styles = {} },
-  { name = "LazySpecial", fg = c.gold, bg = c.terminal, styles = {} },
-  { name = "LazyReasonPlugin", fg = c.green, bg = c.terminal, styles = {} },
-}
--- lua/dist/lazy.lua
+++ lua/dist/lazy.lua
@@ -1,6 +1,15 @@
 return {
   {
     "LazyVim/LazyVim",
+    opts = function()
+      local c = require("bw.config.colors")
+      local highlights = require("bw.config.highlights")
+      highlights.register({
+        { name = "LazyNormal", fg = nil, bg = c.terminal, styles = {} },
+        { name = "LazySpecial", fg = c.gold, bg = c.terminal, styles = {} },
+        { name = "LazyReasonPlugin", fg = c.green, bg = c.terminal, styles = {} },
+      })
+    end,
     keys = {
       {
         "<esc><esc>",
