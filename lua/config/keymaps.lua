14746f62af219ac9940ee11d816255f19b465349
Brandt Wright <wright.brandt@gmail.com>
2025-04-20 03:54:54 -0400

    feat(keymaps): add convenience keys for window movement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -53,6 +53,11 @@ map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
+map("n", "<leader>wh", "<cmd>wincmd H<cr>", { desc = "Move Window Right" })
+map("n", "<leader>wj", "<cmd>wincmd J<cr>", { desc = "Move Window Doen" })
+map("n", "<leader>wk", "<cmd>wincmd K<cr>", { desc = "Move to the Up" })
+map("n", "<leader>wl", "<cmd>wincmd L<cr>", { desc = "Move to the Left" })
+
 --------------------------------------------------------------------------------
 -- Terminals
 --------------------------------------------------------------------------------
