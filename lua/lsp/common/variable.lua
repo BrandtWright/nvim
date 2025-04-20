ae9cc4002128ef0dfc06fac4792721d637efa9fc
Brandt Wright <wright.brandt@gmail.com>
2025-04-20 18:34:54 -0400

    refac(screen_glasses): clean up treesitter highlights

--- lua/lsp/common/variable.lua
+++ lua/lsp/common/variable.lua
@@ -5,10 +5,4 @@ return {
       vim.cmd("hi! link @variable Identifier")
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      vim.cmd("hi! link @lsp.variable.member Type")
-    end,
-  },
 }
