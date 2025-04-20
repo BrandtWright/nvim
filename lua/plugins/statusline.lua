c0fcb21a104fa43d35ffae536a3cc9478c174141
Brandt Wright <wright.brandt@gmail.com>
2025-04-20 19:58:30 -0400

    refac: remove some superfluous code

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,34 +2,37 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      -- TODO: Use highlights instead
-      local theme = require("lualine.themes.gruvbox_dark")
-      local c = require("bw.config.colors")
-      theme.normal.c.fg = c.white
-      theme.normal.c.bg = c.raisin_black
-      theme.insert.c.fg = c.white
-      theme.insert.c.bg = c.raisin_black
-      theme.visual.c.fg = c.white
-      theme.visual.c.bg = c.raisin_black
-      theme.replace.c.fg = c.white
-      theme.replace.c.bg = c.raisin_black
-      theme.command.c.fg = c.white
-      theme.command.c.bg = c.raisin_black
-      theme.inactive.c.fg = c.white
-      theme.inactive.c.bg = c.raisin_black
+      --- Utility to get color values
+      ---@param group string
+      ---@param attr string
+      local function get_color(group, attr)
+        local hl = vim.api.nvim_get_hl(0, { name = group })
+        if hl and hl[attr] then
+          return string.format("#%06x", hl[attr])
+        end
+      end
 
+      local raisin_black = get_color("dark_panel", "bg")
+      local white = get_color("white", "fg")
+
+      -- TODO: Build up theme from screen_glasses, not gruvbox...
+
+      local theme = require("lualine.themes.gruvbox_dark")
+      theme.normal.c.fg = white
+      theme.normal.c.bg = raisin_black
+      theme.insert.c.fg = white
+      theme.insert.c.bg = raisin_black
+      theme.visual.c.fg = white
+      theme.visual.c.bg = raisin_black
+      theme.replace.c.fg = white
+      theme.replace.c.bg = raisin_black
+      theme.command.c.fg = white
+      theme.command.c.bg = raisin_black
+      theme.inactive.c.fg = white
+      theme.inactive.c.bg = raisin_black
       opts.options.theme = theme
       opts.options.component_separators = { left = "", right = "" }
       opts.options.section_separators = { left = "", right = "" }
-
-      -- Lualine X
-      local has_updates_condition = require("lazy.status").has_updates
-      for _, v in pairs(opts.sections.lualine_x) do
-        -- Lazy Update Indicator Component
-        if v.cond == has_updates_condition then
-          v.color = { fg = c.gold }
-        end
-      end
     end,
   },
 }
