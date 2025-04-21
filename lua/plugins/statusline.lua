ab266cb5acbe00e98b1dc1a976cbfa94ecdd41bd
Brandt Wright <wright.brandt@gmail.com>
2025-04-20 21:44:17 -0400

    feat(lualine): simplify config

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,33 +2,40 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      -- TODO: Remove dependency on gruvbox
-      local highlights = require("bw.util.highlights")
-      local raisin_black = highlights.get_color("dark_panel", "bg")
-      local white = highlights.get_color("white", "fg")
-      local gruvbox_theme = require("lualine.themes.gruvbox_dark")
+      local hl = require("bw.util.highlights").get_color
       local screen_glasses_theme = {
-        normal = { c = { fg = white, bg = raisin_black } },
-        insert = { c = { fg = white, bg = raisin_black } },
-        visual = { c = { fg = white, bg = raisin_black } },
-        replace = { c = { fg = white, bg = raisin_black } },
-        command = { c = { fg = white, bg = raisin_black } },
-        inactive = { c = { fg = white, bg = raisin_black } },
+        normal = {
+          a = { fg = hl("Normal", "bg"), bg = hl("bright_panel", "bg") },
+          b = { fg = hl("panel", "fg"), bg = hl("panel", "bg") },
+          c = { fg = hl("dark_panel", "fg"), bg = hl("dark_panel", "bg") },
+        },
+        insert = {
+          a = { fg = hl("Normal", "bg"), bg = hl("green", "fg") },
+          b = { fg = hl("panel", "fg"), bg = hl("panel", "bg") },
+          c = { fg = hl("dark_panel", "fg"), bg = hl("dark_panel", "bg") },
+        },
+        visual = {
+          a = { fg = hl("Normal", "bg"), bg = hl("blue", "fg") },
+          b = { fg = hl("panel", "fg"), bg = hl("panel", "bg") },
+          c = { fg = hl("dark_panel", "fg"), bg = hl("dark_panel", "bg") },
+        },
+        replace = {
+          a = { fg = hl("Normal", "bg"), bg = hl("red", "fg") },
+          b = { fg = hl("panel", "fg"), bg = hl("panel", "bg") },
+          c = { fg = hl("dark_panel", "fg"), bg = hl("dark_panel", "bg") },
+        },
+        command = {
+          a = { fg = hl("Normal", "bg"), bg = hl("yellow", "fg") },
+          b = { fg = hl("panel", "fg"), bg = hl("panel", "bg") },
+          c = { fg = hl("dark_panel", "fg"), bg = hl("dark_panel", "bg") },
+        },
+        inactive = {
+          a = { fg = hl("dark_panel", "fg"), bg = hl("dark_panel", "bg") },
+          b = { fg = hl("panel", "fg"), bg = hl("panel", "bg") },
+          c = { fg = hl("dark_panel", "fg"), bg = hl("dark_panel", "bg") },
+        },
       }
-      local theme = vim.tbl_deep_extend("force", gruvbox_theme, screen_glasses_theme)
-      theme.normal.c.fg = white
-      theme.normal.c.bg = raisin_black
-      theme.insert.c.fg = white
-      theme.insert.c.bg = raisin_black
-      theme.visual.c.fg = white
-      theme.visual.c.bg = raisin_black
-      theme.replace.c.fg = white
-      theme.replace.c.bg = raisin_black
-      theme.command.c.fg = white
-      theme.command.c.bg = raisin_black
-      theme.inactive.c.fg = white
-      theme.inactive.c.bg = raisin_black
-      opts.options.theme = theme
+      opts.options.theme = screen_glasses_theme
       opts.options.component_separators = { left = "", right = "" }
       opts.options.section_separators = { left = "", right = "" }
     end,
