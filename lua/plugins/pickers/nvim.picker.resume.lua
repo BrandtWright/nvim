6dab150a0ef143186eda6c416aa04b043fc39019
Brandt Wright <wright.brandt@gmail.com>
2025-04-25 23:00:44 -0400

    refac(pickers): more picker migration

--- lua/plugins/pickers/nvim.picker.resume.lua
+++ lua/plugins/pickers/nvim.picker.resume.lua
@@ -1,10 +1,15 @@
 -------------------------------------------------------------------------------
--- Fuzzy find man pages
+-- Resume Picker Operation
 -------------------------------------------------------------------------------
 return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
+
+      -- silly key: moved to leader_s_r_t
+      { "<leader>sR", false },
+
+      -- resume telescope
       { "<leader>srt", require("telescope.builtin").resume, desc = "Telescope Picker" },
     },
   },
