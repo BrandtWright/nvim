7918969fb9f51acef4220582b026b40039f3d179
Brandt Wright <wright.brandt@gmail.com>
2025-04-26 21:23:44 -0400

    refac(pickers): readability improvement

--- lua/dist/snacks.scratch.lua
+++ lua/dist/snacks.scratch.lua
@@ -2,34 +2,12 @@ return {
   {
     "folke/snacks.nvim",
     keys = {
-      --------------------------------------------------------------------------
-      -- Disabled default keys
-      --------------------------------------------------------------------------
-
-      -- Scratch pad picker -> leader-sp
+      -- silly key: scratch pad picker -> leader-sp
       { "<leader>S", false },
-
-      --------------------------------------------------------------------------
-      -- Custom Keymaps
-      --------------------------------------------------------------------------
-
       -- Scratch pad picker
-      {
-        "<leader>sp",
-        function()
-          Snacks.scratch.select()
-        end,
-        desc = "Scratch Pad Picker (Snacks)",
-      },
-
+      { "<leader>sp", Snacks.scratch.select, desc = "Scratch Pad Picker (Snacks)" },
       -- Contextual Scratch pad
-      {
-        "<leader>.",
-        function()
-          Snacks.scratch.open()
-        end,
-        desc = "Contextual Scratch Pad (Snacks)",
-      },
+      { "<leader>.", Snacks.scratch.open, desc = "Contextual Scratch Pad (Snacks)" },
     },
   },
 }
