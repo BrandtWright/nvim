844307b1c613160bc56e1e24be62bbf6e9d458cf
Brandt Wright <wright.brandt@gmail.com>
2025-07-27 19:01:45 -0400

    feat(snote): add key bind for slip picker

--- lua/plugins/slipbox.lua
+++ lua/plugins/slipbox.lua
@@ -5,6 +5,15 @@ return {
       "folke/snacks.nvim",
     },
     name = "slipbox", -- Optional: helps with module resolution
+    keys = {
+      {
+        "<leader>sz",
+        function()
+          vim.cmd("SlipFind")
+        end,
+        desc = "Find slip",
+      },
+    },
     config = function()
       require("user.slipbox").setup({
         slipbox_dir = "~/data/base/slipbox",
