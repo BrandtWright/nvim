cf48ea0da07ea275d75ab25e618c1750e02a5743
Brandt Wright <wright.brandt@gmail.com>
2025-07-30 19:32:28 -0400

    refac(slipbox): separate some concerns

--- lua/user/slipbox/init.lua
+++ lua/user/slipbox/init.lua
@@ -1,56 +1,115 @@
 local M = {}
 
-function M.setup(opts)
-  -- New Slip
-  vim.api.nvim_create_user_command("SlipNew", function()
-    local slip_id = vim.fn.trim(vim.fn.system({ "snote", "-n" }))
-    vim.cmd("enew")
+local state = {
+  loaded = false,
+  config = {},
+}
+
+local function configure_opts(opts)
+  if state.loaded then
+    return
+  end
+  state.config = vim.tbl_deep_extend("force", {
+    slipbox_dir = "",
+  }, opts or {})
+end
+
+local function convert_slip_id_to_slip_path(slip_id)
+  return state.config.slipbox_dir .. "/" .. slip_id .. "/README.md"
+end
+
+M.get_next_slip_id = function()
+  return vim.fn.trim(vim.fn.system({ "snote", "-n" }))
+end
+
+function M.list_slips()
+  local slips = vim.fn.systemlist("snote -l")
+  return slips
+end
+
+M.get_slipbox_path = function()
+  return state.config.slipbox_dir
+end
+
+M.new_slip = function()
+  local slip_id = M.get_next_slip_id()
+  local slip_path = convert_slip_id_to_slip_path(slip_id)
+  vim.cmd("enew")
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.api.nvim_buf_set_name(bufnr, slip_path)
+  vim.api.nvim_set_option_value("buftype", "", { buf = bufnr })
+  vim.api.nvim_set_option_value("swapfile", false, { buf = bufnr })
+  vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = bufnr })
+  vim.api.nvim_set_option_value("filetype", "markdown", { buf = bufnr })
+end
+
+M.edit_slip = function(slip_id)
+  if not slip_id then
+    vim.notify("Invalid argument: slip_id is nil", vim.log.levels.ERROR, { title = "Slipbox" })
+  end
+  if slip_id == "" then
+    vim.notify("Invalid argument: slip_id is empty", vim.log.levels.ERROR, { title = "Slipbox" })
+  end
+  if type(slip_id) ~= "string" then
+    vim.notify("Invalid argument: slip_id expected to be a string", vim.log.levels.ERROR, { title = "Slipbox" })
+  end
+
+  local slip_path = convert_slip_id_to_slip_path(slip_id)
+  if vim.fn.filereadable(slip_path) == 1 then
+    vim.cmd("edit " .. slip_path)
     local bufnr = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_name(bufnr, opts.slipbox_dir .. "/" .. slip_id .. "/README.md")
     vim.api.nvim_set_option_value("buftype", "", { buf = bufnr })
     vim.api.nvim_set_option_value("swapfile", false, { buf = bufnr })
     vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = bufnr })
     vim.api.nvim_set_option_value("filetype", "markdown", { buf = bufnr })
-  end, {
-    nargs = 0,
-    desc = "Create a new slip",
-  })
+  else
+    vim.notify(
+      "Slip with ID '" .. slip_id .. "' does not exist.",
+      vim.log.levels.ERROR,
+      { title = "Slipbox", icon = "" }
+    )
+  end
+end
+
+function M.setup(opts)
+  -- Ensure that opts.slipbox_dir is set, is a string, and points to a valid directory
+  if
+    not opts
+    or type(opts.slipbox_dir) ~= "string"
+    or opts.slipbox_dir == ""
+    or not vim.uv.fs_stat(opts.slipbox_dir)
+    or vim.uv.fs_stat(opts.slipbox_dir).type ~= "directory"
+  then
+    vim.notify(
+      "Slipbox setup requires a valid 'slipbox_dir' option that points to an existing directory.",
+      vim.log.levels.ERROR,
+      { title = "Slipbox" }
+    )
+    return
+  end
+
+  configure_opts(opts)
+
+  -- New Slip
+  vim.api.nvim_create_user_command("SlipNew", M.new_slip, { nargs = 0, desc = "Create a new slip" })
 
   -- Edit Slip
   vim.api.nvim_create_user_command("SlipEdit", function(args)
-    local slip_id = args.args
-    local slip_path = opts.slipbox_dir .. "/" .. slip_id .. "/README.md"
-    if vim.fn.filereadable(slip_path) == 1 then
-      vim.cmd("edit " .. slip_path)
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.api.nvim_set_option_value("buftype", "", { buf = bufnr })
-      vim.api.nvim_set_option_value("swapfile", false, { buf = bufnr })
-      vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = bufnr })
-      vim.api.nvim_set_option_value("filetype", "markdown", { buf = bufnr })
-    else
-      vim.notify(
-        "Slip with ID '" .. slip_id .. "' does not exist.",
-        vim.log.levels.ERROR,
-        { title = "Slipbox", icon = "" }
-      )
-    end
-  end, {
-    nargs = 1,
-    desc = "Edit slip with ID",
-  })
+    M.edit_slip(args.args)
+  end, { nargs = 1, desc = "Edit slip with ID" })
 
   -- Save Slip
   local group = vim.api.nvim_create_augroup("SlipWrite", { clear = true })
   vim.api.nvim_create_autocmd("BufWriteCmd", {
     group = group,
-    pattern = opts.slipbox_dir .. "/*/README.md",
+    pattern = M.get_slipbox_path() .. "/*/README.md",
     callback = function(args)
       local bufnr = args.buf
       local bufname = vim.api.nvim_buf_get_name(bufnr)
-      local relative = bufname:gsub("^" .. opts.slipbox_dir .. "/", "")
+      local relative = bufname:gsub("^" .. M.get_slipbox_path() .. "/", "")
       local slip_id = relative:match("^([%w%-]+)/README%.md$")
       if not slip_id then
-        vim.notify("Invalid slipbox file path.", vim.log.levels.ERROR)
+        vim.notify("Invalid slipbox file path.", vim.log.levels.ERROR, { title = "Slipbox" })
         return
       end
 
@@ -79,7 +138,7 @@ function M.setup(opts)
 
   -- Find Slip
   vim.api.nvim_create_user_command("SlipFind", function()
-    local items = {}
+    -- Field Parser
     local function split(str, sep)
       local fields = {}
       for field in string.gmatch(str, "([^" .. sep .. "]+)") do
@@ -88,17 +147,20 @@ function M.setup(opts)
       return fields
     end
 
-    local slips = vim.fn.systemlist("snote -l")
+    -- Slip List
+    local slips = M.list_slips()
+
+    -- Picker Config
+    local items = {}
     for _, v in ipairs(slips) do
       local fields = split(v, "\t")
       local slip_id = fields[1]
       local title = fields[2]
       local tags = fields[3] or ""
-
       table.insert(items, {
         text = title .. " " .. tags,
         name = slip_id,
-        file = opts.slipbox_dir .. "/" .. slip_id .. "/README.md",
+        file = convert_slip_id_to_slip_path(slip_id),
         filetype = "markdown",
         action = function()
           vim.cmd("SlipEdit " .. slip_id)
@@ -106,6 +168,7 @@ function M.setup(opts)
       })
     end
 
+    -- Picker Action
     Snacks.picker({
       items = items,
       source = "Slipbox", -- Optional source name
@@ -127,6 +190,8 @@ function M.setup(opts)
     nargs = 0,
     desc = "Find Slips",
   })
+
+  state.loaded = true
 end
 
 return M
