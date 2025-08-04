3ec49a791b3e446f3be57785f4ad650598111387
Brandt Wright <wright.brandt@gmail.com>
2025-08-04 06:28:53 -0400

    feat(spf): implement relevant colors from screen_glasses
    Colors not aligned with screen_glasses:
        - all dark variants
        - secondary and tertiary colors missing from screen_glasses

--- colors/spf.lua
+++ colors/spf.lua
@@ -17,9 +17,70 @@
 -------------------------------------------------------------------------------
 
 local colors = {
-  red = "#ff0000",
-  yellow = "#ffff00",
-  background = "#000000",
+  foreground = "#ebdbb2",
+  background = "#191816",
+  cursor_line = "#1f1d1b",
+
+  red_dark = "#b25f57",
+  red = "#bd7671",
+  red_bright = "#ffa099",
+
+  orange_dark = "#a1633f",
+  orange = "#ce9178",
+  orange_bright = "#f29950",
+
+  yellow_dark = "#b9b237",
+  yellow = "#c2c27c",
+  yellow_bright = "#fcfca4",
+
+  chartreuse_dark = "#758e57",
+  chartreuse = "#91b76b",
+  chartreuse_bright = "#cfeaa0",
+
+  green_dark = "#4b6550",
+  green = "#769482",
+  green_bright = "#b9d9b8",
+
+  spring_dark = "#4f766e",
+  spring = "#6da89e",
+  spring_bright = "#a9d5c8",
+
+  cyan_dark = "#536a71",
+  cyan = "#7b919e",
+  cyan_bright = "#a7cacc",
+
+  azure_dark = "#45667e",
+  azure = "#5f87a6",
+  azure_bright = "#a2c5e4",
+
+  blue_dark = "#3f6999",
+  blue = "#5692db",
+  blue_bright = "#77b3fc",
+
+  violet_dark = "#59497c",
+  violet = "#8a7b9e",
+  violet_bright = "#eebaff",
+
+  magenta_dark = "#5f4469",
+  magenta = "#8f7099",
+  magenta_bright = "#c99fd6",
+
+  rose_dark = "#94586b",
+  rose = "#c08081",
+  rose_bright = "#e084a3",
+
+  black_dark = "#121210",
+  black = "#0e0d0c",
+  black_bright = "#737373",
+
+  white_dark = "#a89f91",
+  white = "#ebdbb2",
+  white_bright = "#f2e8ce",
+
+  ui_primary_foreground = "#a6977c",
+  ui_primary_background = "#2c2826",
+  ui_secondary_background = "#504945",
+  ui_tertiary_background = "#a89984",
 }
 
 local highlights = {
