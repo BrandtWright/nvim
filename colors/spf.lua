f47a8aeacddb55b997019fcba1ab7a596e573401
Brandt Wright <wright.brandt@gmail.com>
2025-08-07 13:47:26 -0400

    feat(spf): add scaffolding for treesitter-highlight-groups

--- colors/spf.lua
+++ colors/spf.lua
@@ -871,6 +871,280 @@ local derived_links = {
   ["@lsp.mod.static"] = {},
 
   ------------------------------------------------------------------------------
+  -- Treesitter Highlight Groups        see: :help 'treesitter-highlight-groups'
+  ------------------------------------------------------------------------------
+
+  -- various variable names
+  ["@variable"] = {},
+
+  -- built-in variable names (e.g. `this`, `self`)
+  ["@variable.builtin"] = {},
+
+  -- parameters of a function
+  ["@variable.parameter"] = {},
+
+  -- special parameters (e.g. `_`, `it`)
+  ["@variable.parameter.builtin"] = {},
+
+  -- object and struct fields
+  ["@variable.member"] = {},
+
+  -- constant identifiers
+  ["@constant"] = {},
+
+  -- built-in constant values
+  ["@constant.builtin"] = {},
+
+  -- constants defined by the preprocessor
+  ["@constant.macro"] = {},
+
+  -- modules or namespaces
+  ["@module"] = {},
+
+  -- built-in modules or namespaces
+  ["@module.builtin"] = {},
+
+  -- `GOTO` and other labels (e.g. `label:` in C), including heredoc labels
+  ["@label"] = {},
+
+  -- string literals
+  ["@string"] = {},
+
+  -- string documenting code (e.g. Python docstrings)
+  ["@string.documentation"] = {},
+
+  -- regular expressions
+  ["@string.regexp"] = {},
+
+  -- escape sequences
+  ["@string.escape"] = {},
+
+  -- other special strings (e.g. dates)
+  ["@string.special"] = {},
+
+  -- symbols or atoms
+  ["@string.special.symbol"] = {},
+
+  -- filenames
+  ["@string.special.path"] = {},
+
+  -- URIs (e.g. hyperlinks)
+  ["@string.special.url"] = {},
+
+  -- character literals
+  ["@character"] = {},
+
+  -- special characters (e.g. wildcards)
+  ["@character.special"] = {},
+
+  -- boolean literals
+  ["@boolean"] = {},
+
+  -- numeric literals
+  ["@number"] = {},
+
+  -- floating-point number literals
+  ["@number.float"] = {},
+
+  -- type or class definitions and annotations
+  ["@type"] = {},
+
+  -- built-in types
+  ["@type.builtin"] = {},
+
+  -- identifiers in type definitions (e.g. `typedef <type> <identifier>` in C)
+  ["@type.definition"] = {},
+
+  -- attribute annotations (e.g. Python decorators, Rust lifetimes)
+  ["@attribute"] = {},
+
+  -- builtin annotations (e.g. `@property` in Python)
+  ["@attribute.builtin"] = {},
+
+  -- the key in key/value pairs
+  ["@property"] = {},
+
+  -- function definitions
+  ["@function"] = {},
+
+  -- built-in functions
+  ["@function.builtin"] = {},
+
+  -- function calls
+  ["@function.call"] = {},
+
+  -- preprocessor macros
+  ["@function.macro"] = {},
+
+  -- method definitions
+  ["@function.method"] = {},
+
+  -- method calls
+  ["@function.method.call"] = {},
+
+  -- constructor calls and definitions
+  ["@constructor"] = {},
+
+  -- symbolic operators (e.g. `+`, `*`)
+  ["@operator"] = {},
+
+  -- keywords not fitting into specific categories
+  ["@keyword"] = {},
+
+  -- keywords related to coroutines (e.g. `go` in Go, `async/await` in Python)
+  ["@keyword.coroutine"] = {},
+
+  -- keywords that define a function (e.g. `func` in Go, `def` in Python)
+  ["@keyword.function"] = {},
+
+  -- operators that are English words (e.g. `and`, `or`)
+  ["@keyword.operator"] = {},
+
+  -- keywords for including or exporting modules (e.g. `import`, `from` in Python)
+  ["@keyword.import"] = {},
+
+  -- keywords describing namespaces and composite types (e.g. `struct`, `enum`)
+  ["@keyword.type"] = {},
+
+  -- keywords modifying other constructs (e.g. `const`, `static`, `public`)
+  ["@keyword.modifier"] = {},
+
+  -- keywords related to loops (e.g. `for`, `while`)
+  ["@keyword.repeat"] = {},
+
+  -- keywords like `return` and `yield`
+  ["@keyword.return"] = {},
+
+  -- keywords related to debugging
+  ["@keyword.debug"] = {},
+
+  -- keywords related to exceptions (e.g. `throw`, `catch`)
+  ["@keyword.exception"] = {},
+
+  -- keywords related to conditionals (e.g. `if`, `else`)
+  ["@keyword.conditional"] = {},
+
+  -- ternary operator (e.g. `?`, `:`)
+  ["@keyword.conditional.ternary"] = {},
+
+  -- various preprocessor directives and shebangs
+  ["@keyword.directive"] = {},
+
+  -- preprocessor definition directives
+  ["@keyword.directive.define"] = {},
+
+  -- delimiters (e.g. `;`, `.`, `,`)
+  ["@punctuation.delimiter"] = {},
+
+  -- brackets (e.g. `()`, `{}`, `[]`)
+  ["@punctuation.bracket"] = {},
+
+  -- special symbols (e.g. `{}` in string interpolation)
+  ["@punctuation.special"] = {},
+
+  -- line and block comments
+  ["@comment"] = {},
+
+  -- comments documenting code
+  ["@comment.documentation"] = {},
+
+  -- error-type comments (e.g. `ERROR`, `FIXME`, `DEPRECATED`)
+  ["@comment.error"] = {},
+
+  -- warning-type comments (e.g. `WARNING`, `FIX`, `HACK`)
+  ["@comment.warning"] = {},
+
+  -- todo-type comments (e.g. `TODO`, `WIP`)
+  ["@comment.todo"] = {},
+
+  -- note-type comments (e.g. `NOTE`, `INFO`, `XXX`)
+  ["@comment.note"] = {},
+
+  -- bold text
+  ["@markup.strong"] = {},
+
+  -- italic text
+  ["@markup.italic"] = {},
+
+  -- struck-through text
+  ["@markup.strikethrough"] = {},
+
+  -- underlined text (only for literal underline markup!)
+  ["@markup.underline"] = {},
+
+  -- headings, titles (including markers)
+  ["@markup.heading"] = {},
+
+  -- top-level heading
+  ["@markup.heading.1"] = {},
+
+  -- section heading
+  ["@markup.heading.2"] = {},
+
+  -- subsection heading
+  ["@markup.heading.3"] = {},
+
+  -- and so on
+  ["@markup.heading.4"] = {},
+
+  -- and so forth
+  ["@markup.heading.5"] = {},
+
+  -- six levels ought to be enough for anybody
+  ["@markup.heading.6"] = {},
+
+  -- block quotes
+  ["@markup.quote"] = {},
+
+  -- math environments (e.g. `$ ... $` in LaTeX)
+  ["@markup.math"] = {},
+
+  -- text references, footnotes, citations, etc.
+  ["@markup.link"] = {},
+
+  -- link, reference descriptions
+  ["@markup.link.label"] = {},
+
+  -- URL-style links
+  ["@markup.link.url"] = {},
+
+  -- literal or verbatim text (e.g. inline code)
+  ["@markup.raw"] = {},
+
+  -- literal or verbatim text as a stand-alone block
+  ["@markup.raw.block"] = {},
+
+  -- list markers
+  ["@markup.list"] = {},
+
+  -- checked todo-style list markers
+  ["@markup.list.checked"] = {},
+
+  -- unchecked todo-style list markers
+  ["@markup.list.unchecked"] = {},
+
+  -- added text (for diff files)
+  ["@diff.plus"] = {},
+
+  -- deleted text (for diff files)
+  ["@diff.minus"] = {},
+
+  -- changed text (for diff files)
+  ["@diff.delta"] = {},
+
+  -- XML-style tag names (e.g. in XML, HTML, etc.)
+  ["@tag"] = {},
+
+  -- builtin tag names (e.g. HTML5 tags)
+  ["@tag.builtin"] = {},
+
+  -- XML-style tag attributes
+  ["@tag.attribute"] = {},
+
+  -- XML-style tag delimiters
+  ["@tag.delimiter"] = {},
+
+  ------------------------------------------------------------------------------
   -- Other Common Highlights
   ------------------------------------------------------------------------------
 
