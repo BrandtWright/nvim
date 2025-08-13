c86bfdae0e685b5089e37af8757705666f505f6f
Brandt Wright <wright.brandt@gmail.com>
2025-08-13 15:23:56 -0400

    fix(bw.util.highlights): make get_attribute() follow links

--- lua/bw/util/highlights.lua
+++ lua/bw/util/highlights.lua
@@ -3,7 +3,7 @@ local M = {}
 ---@param group string
 ---@param attr string
 M.get_attribute = function(group, attr)
-  local hl = vim.api.nvim_get_hl(0, { name = group })
+  local hl = vim.api.nvim_get_hl(0, { name = group, link = false })
   if hl and hl[attr] then
     return string.format("#%06x", hl[attr])
   end
