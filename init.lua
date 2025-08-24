09ddf6984fc32908655a6d05f65fd4efb1ade4d0
Brandt Wright <wright.brandt@gmail.com>
2025-08-24 11:45:54 -0400

    refac(lsp): move lsp plugin config into plugins folder

--- init.lua
+++ init.lua
@@ -29,11 +29,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
-    { import = "dist" },
     { import = "plugins" },
     { import = "plugins/pickers" },
     { import = "plugins/toggles" },
-    { import = "lsp" },
+    { import = "plugins/lsp" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
